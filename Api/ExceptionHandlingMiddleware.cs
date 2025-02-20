039b0e572ba9a625654409582e36a03450aa8e2b
Alvaro Kú <alvaro.ku@grupoblueocean.com>
2025-02-19 19:02:07 -0600

    middleware para manejo de excepciones

---
+++ Api/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace Api
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var response = new
+            {
+                message = $"Mensaje: {exception.Message} Detalles: {exception.InnerException?.Message}",
+            };
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
